100662c9e636f078671e17b9c88c47295f9b7621
Nanami Tomita <drakee103dh@gmail.com>
2018-03-02 21:18:01 +0900

    up

--- _slide2/js/top.js
+++ _slide2/js/top.js
@@ -1,49 +1,165 @@
 // 画像を保存する配列
 var manifest = [
+    //左上
     {src: 'img/top/l1-a.png'},
     {src: 'img/top/l1-c.png'},
-    // {src: 'img/top/l1-e.png'},
-    {src: 'img/top/l1-g.png'},  //左上
-
+    {src: 'img/top/l1-e.png'},  //2 使用しない
+    {src: 'img/top/l1-g.png'},
+    //右上
     {src: 'img/top/r1-b.png'},
-    // {src: 'img/top/r1-d.png'},
+    {src: 'img/top/r1-d.png'},  //5 使用しない
     {src: 'img/top/r1-f.png'},
-    {src: 'img/top/r1-h.png'},  //右上
-
+    {src: 'img/top/r1-h.png'},
+    //左中央
     {src: 'img/top/l2-a.png'},
     {src: 'img/top/l2-c.png'},
     {src: 'img/top/l2-e.png'},
-    // {src: 'img/top/l2-g.png'},  //左中央
-
+    {src: 'img/top/l2-g.png'},  //11 使用しない
+    //右中央
     {src: 'img/top/r2-b.png'},
     {src: 'img/top/r2-d.png'},
-    // {src: 'img/top/r2-f.png'},
-    {src: 'img/top/r2-h.png'},  //右中央
-
+    {src: 'img/top/r2-f.png'},  //14 使用しない
+    {src: 'img/top/r2-h.png'},
+    //左下
     {src: 'img/top/l3-a.png'},
-    // {src: 'img/top/l3-c.png'},
+    {src: 'img/top/l3-c.png'},  //17 使用しない
     {src: 'img/top/l3-e.png'},
-    {src: 'img/top/l3-g.png'},  //左下
-
+    {src: 'img/top/l3-g.png'},
+    //右下
     {src: 'img/top/r3-b.png'},
     {src: 'img/top/r3-d.png'},
-    {src: 'img/top/r3-f.png'}
-    // {src: 'img/top/r3-h.png'},  //右下
+    {src: 'img/top/r3-f.png'},
+    {src: 'img/top/r3-h.png'}  //23 使用しない
 ];
 
+//preload-----------------------------------------------
 $(function(){
+
+    var windowWidth;
+    var windowHeight;
+
+    // console.log('preload----------');
+
+    var wLoader = $('.wrap-loader');
+
+
+    // loadQueueクラス
+    var loadQueue = new createjs.LoadQueue();
+
+    // 同時に何ファイル読み込むか
+    loadQueue.setMaxConnections(6);
+
+    // 読み込み開始
+    loadQueue.loadManifest(manifest);
+
+    // 読み込み状況
+    loadQueue.addEventListener('progress',function(evt){
+        console.log(evt.progress);
+    });
+
+    // ひとつ読み込み終わったら
+    loadQueue.addEventListener("fileload", function(evt){
+        // console.log(evt.result);
+    });
+
+    // 全部読み込み終わったら
+    loadQueue.addEventListener("complete", function(evt){
+        console.log('読み込み完了');
+        startScene();
+    });
+
+
+
+    function startScene(){
+        wLoader.addClass('fade-out');
+        setTimeout(function(){
+            wLoader.remove();
+            $('.container').css('display','block');
+
+        },1500)
+
+        puzzleStart();
+
+    }
+
+    function puzzleStart(){
+        //たいやき1 (インスタンス)
+        var puzzle1 = new Puzzle(photoArr01,0,'#slider1',0);
+        // -----------------------------------------------
+        // イベント
+        // -----------------------------------------------
+        $('#rightBtn1').on('click',function(){  //ボタンをクリックしたら
+            puzzle1.clickAnime();
+        });
+
+        //たいやき2 (インスタンス)
+        var puzzle2 = new Puzzle(photoArr02,0,'#slider2',1);
+        // -----------------------------------------------
+        // イベント
+        // -----------------------------------------------
+        $('#rightBtn2').on('click',function(){  //ボタンをクリックしたら
+            puzzle2.clickAnime();
+        });
+
+        //たいやき3 (インスタンス)
+        var puzzle3 = new Puzzle(photoArr03,0,'#slider3',2);
+        // -----------------------------------------------
+        // イベント
+        // -----------------------------------------------
+        $('#rightBtn3').on('click',function(){  //ボタンをクリックしたら
+            puzzle3.clickAnime();
+        });
+
+        //たいやき4 (インスタンス)
+        var puzzle4 = new Puzzle(photoArr04,0,'#slider4',3);
+        // -----------------------------------------------
+        // イベント
+        // -----------------------------------------------
+        $('#rightBtn4').on('click',function(){  //ボタンをクリックしたら
+            puzzle4.clickAnime();
+        });
+
+        //たいやき5 (インスタンス)
+        var puzzle5 = new Puzzle(photoArr05,0,'#slider5',4);
+        // -----------------------------------------------
+        // イベント
+        // -----------------------------------------------
+        $('#rightBtn5').on('click',function(){  //ボタンをクリックしたら
+            puzzle5.clickAnime();
+        });
+
+        //たいやき6 (インスタンス)
+        var puzzle6 = new Puzzle(photoArr06,0,'#slider6',5);
+        // -----------------------------------------------
+        // イベント
+        // -----------------------------------------------
+        $('#rightBtn6').on('click',function(){  //ボタンをクリックしたら
+            puzzle6.clickAnime();
+        });
+    }
+
+
+
+
+
+
+
+
+
+//puzzle---------------------------------------------------------------
+
 
     // パズル全体を管理する配列全部が1になると揃う
     var puzzleArr = [0,0,0,0,0,0];
 
 
     // 画像の元の配列  ひとつめのパズルで使う画像はこれだよ
-    var photoArr01 = [manifest[0].src,manifest[1].src,manifest[2].src];
-    var photoArr02 = [manifest[3].src,manifest[4].src,manifest[5].src];
-    var photoArr03 = [manifest[6].src,manifest[7].src,manifest[8].src];
-    var photoArr04 = [manifest[9].src,manifest[10].src,manifest[11].src];
-    var photoArr05 = [manifest[12].src,manifest[13].src,manifest[14].src];
-    var photoArr06 = [manifest[15].src,manifest[16].src,manifest[17].src];
+    var photoArr01 = [manifest[0].src,manifest[1].src,manifest[3].src];
+    var photoArr02 = [manifest[4].src,manifest[6].src,manifest[7].src];
+    var photoArr03 = [manifest[8].src,manifest[9].src,manifest[10].src];
+    var photoArr04 = [manifest[12].src,manifest[13].src,manifest[15].src];
+    var photoArr05 = [manifest[16].src,manifest[18].src,manifest[19].src];
+    var photoArr06 = [manifest[20].src,manifest[21].src,manifest[22].src];
 
 
     //たいやき機
@@ -181,92 +297,40 @@ $(function(){
 
 
     function machCount(){
-        if(countArray[0] == 0  && countArray[1] == 0 && countArray[2] == 0 && countArray[3] == 0 && countArray[4] == 0 && countArray[5] == 0){
 
-            $('#slider135-container').addClass('openLeft');  //パズル左コンテナ移動
-            $('#slider246-container').addClass('openRight');  //パズル右コンテナ移動
+
+        $('#slider135-container').addClass('openLeft');  //パズル左コンテナ移動
+        $('#slider246-container').addClass('openRight');  //パズル右コンテナ移動
+
+        setTimeout(function(){
+            $('.close-container').remove();  //閉じ鍵消す
 
             setTimeout(function(){
-                $('.close-container').remove();  //閉じ鍵消す
+                $('.open-container').addClass('fade-out');  //開き鍵フェードアウト
 
                 setTimeout(function(){
-                    $('.open-container').addClass('fade-out');  //開き鍵フェードアウト
+                    $('.footer').addClass('fade-out');  //footerフェードアウト
 
                     setTimeout(function(){
-                        $('.footer').addClass('fade-out');  //footerフェードアウト
+                        $('.contents').css('display','block').css('height','2270px');  //home出現&homeの高さを2270pxに
 
                         setTimeout(function(){
-                            $('.contents').css('display','block').css('height','2270px');  //home出現&homeの高さを2270pxに
+                            $('.puzzle').addClass('fade-out'); //puzzleをコンテナごとフェードアウト
 
                             setTimeout(function(){
-                                $('.puzzle').addClass('fade-out'); //puzzleをコンテナごとフェードアウト
+                                $('.footer').addClass('fade-in-footer');  //footerフェードイン
 
-                                setTimeout(function(){
-                                    $('.footer').addClass('fade-in-footer');  //footerフェードイン
+                            },10);  //footerフェードインのかっこ
+                        },1000);//puzzleフェードアウトのかっこ
+                    },2000);//home出現のかっこ
+                },10);  //footerフェードアウトのかっこ
+            },500);//開き鍵フェードアウトのかっこ
+        },1500);  //閉じ鍵消すかっこ
 
-                                },10);  //footerフェードインのかっこ
-                            },1000);//puzzleフェードアウトのかっこ
-                        },2000);//home出現のかっこ
-                    },10);  //footerフェードアウトのかっこ
-                },500);//開き鍵フェードアウトのかっこ
-            },1500);  //閉じ鍵消すかっこ
-        }  //if文のかっこ
     }
 
 
-    //たいやき1 (インスタンス)
-    var puzzle1 = new Puzzle(photoArr01,0,'#slider1',0);
-    // -----------------------------------------------
-    // イベント
-    // -----------------------------------------------
-    $('#rightBtn1').on('click',function(){  //ボタンをクリックしたら
-        puzzle1.clickAnime();
-    });
 
-    //たいやき2 (インスタンス)
-    var puzzle2 = new Puzzle(photoArr02,0,'#slider2',1);
-    // -----------------------------------------------
-    // イベント
-    // -----------------------------------------------
-    $('#rightBtn2').on('click',function(){  //ボタンをクリックしたら
-        puzzle2.clickAnime();
-    });
-
-    //たいやき3 (インスタンス)
-    var puzzle3 = new Puzzle(photoArr03,0,'#slider3',2);
-    // -----------------------------------------------
-    // イベント
-    // -----------------------------------------------
-    $('#rightBtn3').on('click',function(){  //ボタンをクリックしたら
-        puzzle3.clickAnime();
-    });
-
-    //たいやき4 (インスタンス)
-    var puzzle4 = new Puzzle(photoArr04,0,'#slider4',3);
-    // -----------------------------------------------
-    // イベント
-    // -----------------------------------------------
-    $('#rightBtn4').on('click',function(){  //ボタンをクリックしたら
-        puzzle4.clickAnime();
-    });
-
-    //たいやき5 (インスタンス)
-    var puzzle5 = new Puzzle(photoArr05,0,'#slider5',4);
-    // -----------------------------------------------
-    // イベント
-    // -----------------------------------------------
-    $('#rightBtn5').on('click',function(){  //ボタンをクリックしたら
-        puzzle5.clickAnime();
-    });
-
-    //たいやき6 (インスタンス)
-    var puzzle6 = new Puzzle(photoArr06,0,'#slider6',5);
-    // -----------------------------------------------
-    // イベント
-    // -----------------------------------------------
-    $('#rightBtn6').on('click',function(){  //ボタンをクリックしたら
-        puzzle6.clickAnime();
-    });
 
 
 });
