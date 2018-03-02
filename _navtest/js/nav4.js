63e48253164e30ff99a7043fd0d5f0b5945f766e
Nanami Tomita <drakee103dh@gmail.com>
2018-03-02 15:53:58 +0900

    up

--- _navtest/js/nav4.js
+++ _navtest/js/nav4.js
@@ -3,3 +3,27 @@ $('.header-container').find('.menu').on('click',function(){
     $('.home').css('display','none');
     $('.menu-container').css('display','block').addClass('fade-in')
 });
+
+
+
+var windowWidth;
+var windowHeight;
+
+    // windowをリサイズしたら
+    $(window).on('resize',function(){
+        console.log('リサイズ');
+
+        resizeWindow();
+
+
+    });
+
+
+function resizeWindow(){
+    windowWidth = $(window).width();
+    windowHeight = $(window).height();
+
+    $('.menu-container').width(windowWidth);
+    $('.menu-container').height(windowHeight);
+
+}
