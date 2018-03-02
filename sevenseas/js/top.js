73f01309a460f2e32ad0d8d08d4eeb324c48a213
Nanami Tomita <drakee103dh@gmail.com>
2018-03-02 21:58:35 +0900

    up

--- sevenseas/js/top.js
+++ sevenseas/js/top.js
@@ -62,8 +62,44 @@ var splashSprite = {
 //preload-----------------------------------------------
 $(function(){
 
-    var windowWidth;
-    var windowHeight;
+    $('.header-container').find('.menu').on('click',function(){
+    console.log('くりっく');
+    $('.home').css('display','none');
+    $('.menu-container').css('display','block').removeClass('fade-out').addClass('fade-in');
+});
+
+$('.w-menu').on('click',function(){
+    console.log('くりっく2');
+    $('.home').css('display','block');
+    $('.menu-container').removeClass('fade-in').addClass('fade-out');
+    setTimeout(function(){
+        $('.menu-container').removeClass('fade-in').css('display','none');
+    },1000);
+});
+
+
+
+var windowWidth;
+var windowHeight;
+
+    // windowをリサイズしたら
+    $(window).on('resize',function(){
+        console.log('リサイズ');
+
+        resizeWindow();
+
+
+    });
+
+
+function resizeWindow(){
+    windowWidth = $(window).width();
+    windowHeight = $(window).height();
+
+    $('.menu-container').width(windowWidth);
+    $('.menu-container').height(windowHeight);
+
+}
 
     // console.log('preload----------');
 
